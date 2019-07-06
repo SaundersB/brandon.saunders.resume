5d82f0d8676a97c4f5b984bf80c7b04189ca7a73
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-07-05 20:59:06 -0700

    Adding educational history.

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -3,8 +3,12 @@
         <div>
             Education
         </div>
-        <div class="col">
-
+        <div class="block" v-for="education in this.education" :key="education.degree">
+            <div class="block-helper"></div>
+            <div class="headline">{{education.degree}}</div>
+            <p class="info">
+                {{education.timeperiod}}, {{education.description}}
+            </p>
         </div>
     </div>
 </template>
@@ -12,6 +16,17 @@
 <script>
     export default {
         name: 'Education',
+        data: function() {
+            return {
+                education: [
+                    {
+                        degree: 'Bachelor of Science',
+                        timeperiod: 'June 2013 - June 2016',
+                        description: 'Major in Computer Science, California State University of San Bernardino',
+                    },
+                ]
+            }
+        }
     };
 </script>
 
